0a524c14b3948e4efa8abd794751da0f275b11ff
Mac <Mac>
2020-08-09 20:57:01 +0530

    Initial Commit

---
+++ News App WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  News App WatchKit Extension
+//
+//  Created by Mac HD on 09/08/20.
+//  Copyright © 2020 Mac HD. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ News App WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  News App WatchKit Extension
+//
+//  Created by Mac HD on 09/08/20.
+//  Copyright © 2020 Mac HD. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
